58ec74895100ef3a8df8bbb2847d1277fd5ff130
Clarence005 <clarencesamuel008@gmail.com>
2024-09-25 11:26:35 +0530

    updated

---
+++ secret/hidden/superhidden/api/key.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+    const { key1} = req.body; // match frontend
+
+    const validKey1 = "CYPHERCHASE(Ransomware)";
+    
+    // Check the key combinations
+    if ((key1 === validKey1 )) {
+        return res.status(200).json({ success: true, redirectUrl: 'https://website-2-ctf.vercel.app/' });
+    } else {
+        return res.status(401).json({ success: false, redirectUrl: '/w22.html' });
+    }
+}
